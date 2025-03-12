4f625658c8b569a00a52c076f73794f6529f19dc
Vinícius Madureira Godinho de Oliveira <vinimagod@gmail.com>
2025-03-12 20:42:47 -0300

    Adiciona classe base Veiculo

---
+++ src/veiculos/Veiculo.java
@@ -0,0 +1,18 @@
+package src.veiculos;
+
+public class Veiculo{
+
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano);
+    }
+}
